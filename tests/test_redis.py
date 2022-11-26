1143b7f7088659f69d91fda32f02da01a440a4ad
Slava Loievskyi <loievskyi.slava@gmail.com>
2022-11-26 16:33:51 +0200

    Add tests for RedisClientTaskHelper.add_task_to_queue

--- tests/test_redis.py
+++ tests/test_redis.py
@@ -1,4 +1,7 @@
+import os
 import unittest
+import pickle
+
 import redis
 
 from task_helper.redis import (
@@ -8,6 +11,10 @@ from task_helper.redis import (
 
 
 class TestFullQueueNameMixin(unittest.TestCase):
+    """
+    Tests to make sure that _get_ful_queue_name is calculating correctly.
+    """
+
     def setUp(self):
         self.mixin = FullQueueNameMixin()
 
@@ -35,3 +42,80 @@ class TestFullQueueNameMixin(unittest.TestCase):
         name = self.mixin._get_ful_queue_name("test_queue_name")
         self.assertEqual(name, "test_queue_name")
 
+
+class RedisSetupMixin:
+    """
+    mixin for redis connection initializing.
+    """
+
+    def setUp(self):
+        redis_host = os.environ.get("REDIS_HOST")
+        redis_port = os.environ.get("REDIS_PORT")
+        redis_db = os.environ.get("REDIS_DB")
+        redis_password = os.environ.get("REDIS_PASSWORD", None)
+
+        assert redis_host is not None, "redis_host is None"
+        assert redis_port is not None, "redis_port is None"
+        assert redis_db is not None, "redis_db is None"
+
+        self.redis_connection = redis.Redis(
+            host=redis_host,
+            port=redis_port,
+            db=redis_db,
+            password=redis_password)
+        self.redis_connection.flushdb()
+        self.task_helper = RedisClientTaskHelper(
+            redis_connection=self.redis_connection)
+
+
+def test_func(text):
+    return f"{text}{text}"
+
+
+class TestRedisClientTaskHelper(RedisSetupMixin, unittest.TestCase):
+    """
+    Tests to make sure that RedisClientTaskHelper is working correctly.
+    """
+
+    def setUp(self):
+        super().setUp()
+
+    def test_add_task_to_queue_as_dict(self):
+        start_task_data = {
+            "test_key": "test_value",
+            "int": 123,
+        }
+        start_task_id = self.task_helper.add_task_to_queue(
+            queue_name="test_queue_name",
+            task_data=start_task_data)
+
+        task = self.redis_connection.lpop(
+            self.task_helper._get_ful_queue_name(
+                queue_name="test_queue_name", sufix="pending")
+        )
+
+        self.assertIsNotNone(task)
+        task_id, task_data = pickle.loads(task)
+        self.assertEqual(start_task_id, task_id)
+        self.assertDictEqual(start_task_data, task_data)
+
+    def test_add_task_to_queue_as_func(self):
+        start_task_data = test_func
+        start_task_id = self.task_helper.add_task_to_queue(
+            queue_name="test_queue_name",
+            task_data=start_task_data)
+
+        task = self.redis_connection.lpop(
+            self.task_helper._get_ful_queue_name(
+                queue_name="test_queue_name", sufix="pending")
+        )
+
+        self.assertIsNotNone(task)
+        task_id, task_data = pickle.loads(task)
+        self.assertEqual(start_task_id, task_id)
+        self.assertEqual(start_task_data, task_data)
+        self.assertEqual(task_data("123"), "123123")
+
+
+if __name__ == "__main__":
+    unittest.main()
