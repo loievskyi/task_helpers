610576cd2e931e478afabcee1e10a6c0306c62a3
Slava Loievskyi <loievskyi.slava@gmail.com>
2022-12-14 22:39:32 +0200

    Fix: Task.input_data or Task.function are required now; added queue_name argument

--- task_helpers/tasks.py
+++ task_helpers/tasks.py
@@ -10,16 +10,19 @@ class Task:
     """
 
     def __init__(
-            self, id=None, data=None, result=None,
+            self, queue_name, id=None, input_data=None, result=None,
             function=None, function_args=None, function_kwargs=None):
         """
         Class initialization. All argumengs are optional.
         """
+        assert input_data is not None or function is not None, \
+            "\"input_data\" or \"function\" arguments must be specified"
+
         self.id = id or uuid.uuid4()
         self.function = function
         self.function_args = function_args or tuple()
         self.function_kwargs = function_kwargs or dict()
-        self.data = data
+        self.input_data = input_data
         self.result = result
         self.started_at = datetime.datetime.utcnow()
 
