ff3d24fce8cea84bcea893dbfbf1dade1d62e8d2
Slava Loievskyi <loievskyi.slava@gmail.com>
2022-12-16 23:10:26 +0200

    version iteration

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read_file(file_path):
 
 setup(
     name="task_helpers",
-    version="0.0.7",
+    version="0.0.8",
     url="https://github.com/loievskyi/task_helpers",
     license="BSD",
     description="A package for creating task helpers.",
