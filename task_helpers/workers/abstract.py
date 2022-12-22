64032d8fcadeaf02e0b5aeb33f86e527613fa09e
Slava Loievskyi <loievskyi.slava@gmail.com>
2022-12-22 23:30:55 +0200

    abstract worker with Task instance

--- task_helpers/workers/abstract.py
+++ task_helpers/workers/abstract.py
@@ -1,28 +1,32 @@
+from typing import List
+
+from task_helpers.tasks import Task
+
+
 class AbstractWorker:
     """
     An abstract class for task processing.
     """
 
-    def wait_for_tasks(self, *args, **kwargs):
+    def wait_for_tasks(self, *args, **kwargs) -> List(Task):
         """
         Abstract method. Should return a list of tasks as a list.
         """
         raise NotADirectoryError
 
-    def perform_tasks(self, tasks, *args, **kwargs):
+    def perform_tasks(self, tasks: List(Task), *args, **kwargs) -> List(Task):
         """
-        Abstract method for processing tasks. Should return a list of tasks:
-        [(task_id, task_result), (task_id, task_result), ...]
+        Abstract method for processing tasks.
         """
         raise NotImplementedError
 
-    def return_task_results(self, tasks, *args, **kwargs):
+    def return_task_results(self, tasks: List(Task), *args, **kwargs) -> None:
         """
         Abstract method for returning task results.
         """
         raise NotImplementedError
 
-    def perform(self, total_iterations: int, *args, **kwargs):
+    def perform(self, total_iterations: int, *args, **kwargs) -> None:
         """
         Abstract method for starting a worker.
         """
