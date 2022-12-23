59ba35905cc3e82e0b5a2930d523ad8229086234
Slava Loievskyi <loievskyi.slava@gmail.com>
2022-12-23 23:22:20 +0200

    Added typing for abstract couriers

--- task_helpers/couriers/abstract.py
+++ task_helpers/couriers/abstract.py
@@ -1,3 +1,6 @@
+from typing import List, Tuple
+
+
 class AbstractClientTaskCourier(object):
     """
     Abstract class for the client side of task helpers.
@@ -10,25 +13,25 @@ class AbstractClientTaskCourier(object):
         - check_for_done - Checks if the task has completed.
     """
 
-    def get_task_result(self, queue_name, task_id, delete_data=True):
+    def get_task_result(self, queue_name, task_id, delete_data=True) -> object:
         """Returns task retuls, if it exists.
         Otherwise, raises exceptions.TaskResultDoesNotExist
         Client side method."""
         raise NotImplementedError
 
-    def wait_for_task_result(
-            self, queue_name, task_id, delete_data=True, timeout=None):
+    def wait_for_task_result(self, queue_name, task_id, delete_data=True,
+                             timeout=None) -> object:
         """Waits for the task result to appear, and then returns it.
         Raises TimeoutError in case of timeout.
         Client side method"""
         raise NotImplementedError
 
-    def add_task_to_queue(self, queue_name, task_data):
-        """Put the task in the queue for processing.
+    def add_task_to_queue(self, queue_name, task_data) -> object:
+        """Put the task in the queue for processing. Returns task_id
         Client side method."""
         raise NotImplementedError
 
-    def check_for_done(self, queue_name, task_id):
+    def check_for_done(self, queue_name, task_id) -> bool:
         """Checks if the task has completed.
         Returns True - if task is done (successful or unsuccessful),
         or False if there is no task result yet."""
@@ -47,26 +50,27 @@ class AbstractWorkerTaskCourier(object):
         - return_task_result - returns result to the client side.
     """
 
-    def get_tasks(self, queue_name, max_count):
+    def get_tasks(self, queue_name, max_count) -> List[Tuple]:
         """Pops tasks from queue and returns it. The number of task which
-        depends on max_count and the number of elements in the queue.
+        depends on max_count and the number of elements in the queue. Tasks are
+        [(task_id, task_data), (task_id, task_data), ...]
         Worker side method."""
         raise NotImplementedError
 
-    def get_task(self, queue_name):
+    def get_task(self, queue_name) -> Tuple:
         """Returns one task from the queue. If task doesn't exists,
-        raises exceptions.TaskDoesNotExist.
+        raises exceptions.TaskDoesNotExist. Task is tuple (task_id, task_data)
         Worker side method."""
         raise NotImplementedError
 
-    def wait_for_task(self, queue_name, timeout=None):
+    def wait_for_task(self, queue_name, timeout=None) -> Tuple:
         """Returns one task from queue as tuple (task_id, task_data).
         If timeout is None (default), then waits task indefinitely.
         Raises TimeoutError in case of timeout.
         Worker side method."""
         raise NotImplementedError
 
-    def return_task_result(self, queue_name, task_id, task_result):
+    def return_task_result(self, queue_name, task_id, task_result) -> None:
         """Return the result of processing the task to the client.
         Worker side method."""
         raise NotImplementedError
