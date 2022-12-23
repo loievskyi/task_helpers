23b5b0ad6f94a3843c5bf363c8fde943e1f4da39
Slava Loievskyi <loievskyi.slava@gmail.com>
2022-12-23 23:35:47 +0200

    Added some tests for BaseWorker methods: wait_for_tasks, __init__ & test_perform_tasks

--- tests/workers/test_base.py
+++ tests/workers/test_base.py
@@ -1,9 +1,10 @@
 import uuid
 import unittest
 
+import timeout_decorator
+
 from task_helpers.couriers.redis import RedisClientWorkerTaskCourier
 from task_helpers.workers.base import BaseWorker
-from task_helpers import exceptions
 from ..mixins import RedisSetupMixin
 
 
@@ -14,154 +15,57 @@ class BaseWorkerTestCadse(RedisSetupMixin, unittest.TestCase):
 
     def setUp(self):
         super().setUp()
+        self.queue_name = "test_queue_name"
         self.task_courier = RedisClientWorkerTaskCourier(self.redis_connection)
-        self.base_worker = BaseWorker(task_courier=self.task_courier)
+        self.worker = BaseWorker(task_courier=self.task_courier)
+
+        # monkey patching
+        self.worker.queue_name = self.queue_name
+        self.worker.max_tasks_per_iteration = 100
+
+    class TimeoutTestException(TimeoutError):
+        pass
 
     def test___init__(self):
-        worker = BaseWorker(task_courier=self.task_courier, zxc="text",
-                            queue_name="new_test_queue_name")
+        worker = BaseWorker(task_courier=self.task_courier,
+                            max_tasks_per_iteration=5,
+                            test_variable="test_variable_data")
         self.assertEqual(worker.task_courier, self.task_courier)
-        self.assertEqual(worker.zxc, "text")
-        self.assertEqual(worker.queue_name, "new_test_queue_name")
+        self.assertEqual(worker.max_tasks_per_iteration, 5)
+        self.assertEqual(worker.test_variable, "test_variable_data")
 
-    def test_perform_task(self):
+    def test_wait_for_tasks(self):
+        before_tasks = []
+        for num in range(10):
+            task_data = f"task_data_{num}"
+            task_id = self.task_courier.add_task_to_queue(
+                queue_name=self.queue_name,
+                task_data=task_data)
+            before_tasks.append((task_id, task_data))
+
+        after_tasks = self.worker.wait_for_tasks()
+        self.assertEqual(len(after_tasks), 10)
+        self.assertListEqual(before_tasks, after_tasks)
+
+    @timeout_decorator.timeout(1, timeout_exception=TimeoutTestException)
+    def test_wait_for_tasks_if_no_tasks(self):
+        with self.assertRaises(self.TimeoutTestException) as context:
+            self.worker.wait_for_tasks()
+        self.assertNotEqual(type(context.exception), TimeoutError)
+
+    def test_wait_for_tasks_if_count_of_tasks_is_greater_than_max_tasks_per_iteration(self):
+        before_tasks = []
+        for num in range(150):
+            task_data = f"task_data_{num}"
+            task_id = self.task_courier.add_task_to_queue(
+                queue_name=self.queue_name,
+                task_data=task_data)
+            before_tasks.append((task_id, task_data))
+
+        after_tasks = self.worker.wait_for_tasks()
+        self.assertEqual(len(after_tasks), 100)
+        self.assertListEqual(before_tasks[:100], after_tasks)
+
+    def test_perform_tasks(self):
         with self.assertRaises(expected_exception=NotImplementedError):
-            self.base_worker.perform_task((uuid.uuid1(), "task_data"))
-
-    def test_perform_if_no_exception(self):
-        def perform_task(task):
-            task_id, task_data = task
-            return str(task_data) + "text"
-
-        queue_name = "test_perform_if_no_exception"
-        task_data = "test_task_data"
-        task_id = self.task_courier.add_task_to_queue(
-            queue_name=queue_name,
-            task_data=task_data)
-
-        # monkey patching
-        self.base_worker.perform_task = perform_task
-        self.base_worker.queue_name = queue_name
-
-        self.base_worker.perform(total_tasks=1)
-        task_result = self.task_courier.wait_for_task_result(
-            queue_name=queue_name,
-            task_id=task_id)
-
-        self.assertEqual(task_result, str(task_data) + "text")
-
-    class CustomException(Exception):
-        def __init__(self, text=None):
-            self.text = text
-
-    def test_perform_if_exception(self):
-
-        def perform_task(task):
-            raise self.CustomException(text="new custom ex text")
-
-        queue_name = "test_perform_if_exception"
-        task_data = "test_task_data"
-        task_id = self.task_courier.add_task_to_queue(
-            queue_name=queue_name,
-            task_data=task_data)
-
-        # monkey patching
-        self.base_worker.perform_task = perform_task
-        self.base_worker.queue_name = queue_name
-
-        self.base_worker.perform(total_tasks=1)
-        task_result = self.task_courier.wait_for_task_result(
-            queue_name=queue_name,
-            task_id=task_id)
-
-        self.assertEqual(type(task_result), exceptions.PerformTaskError)
-        self.assertEqual(type(task_result.exception), self.CustomException)
-        self.assertEqual(task_result.exception.text, "new custom ex text")
-
-    def test_perform_if_return_task_result_True(self):
-        def perform_task(task):
-            task_id, task_data = task
-            return str(task_data) + "text"
-
-        queue_name = "test_perform_if_no_exception"
-        task_data = "test_task_data"
-        task_id = self.task_courier.add_task_to_queue(
-            queue_name=queue_name,
-            task_data=task_data)
-
-        # monkey patching
-        self.base_worker.perform_task = perform_task
-        self.base_worker.queue_name = queue_name
-        self.base_worker.return_task_result = True
-
-        self.base_worker.perform(total_tasks=1)
-        exists_task_result = self.task_courier.check_for_done(
-            queue_name=queue_name,
-            task_id=task_id)
-        task_result = self.task_courier.get_task_result(
-            queue_name=queue_name,
-            task_id=task_id)
-
-        self.assertEqual(exists_task_result, True)
-        self.assertEqual(task_result, str(task_data) + "text")
-
-    def test_perform_if_return_task_result_False(self):
-        def perform_task(task):
-            task_id, task_data = task
-            return str(task_data) + "text"
-
-        queue_name = "test_perform_if_no_exception"
-        task_data = "test_task_data"
-        task_id = self.task_courier.add_task_to_queue(
-            queue_name=queue_name,
-            task_data=task_data)
-
-        # monkey patching
-        self.base_worker.perform_task = perform_task
-        self.base_worker.queue_name = queue_name
-        self.base_worker.return_task_result = False
-
-        self.base_worker.perform(total_tasks=1)
-        exists_task_result = self.task_courier.check_for_done(
-            queue_name=queue_name,
-            task_id=task_id)
-        with self.assertRaises(exceptions.TaskResultDoesNotExist):
-            self.task_courier.get_task_result(
-                queue_name=queue_name,
-                task_id=task_id)
-
-        self.assertEqual(exists_task_result, False)
-
-    # def test_perform_after_iteration_sleep_time(self):
-    #     pass
-    #
-    # def test_perform_total_tasks(self):
-    #     pass
-
-    # GITHUB ISSUE https://github.com/loievskyi/task_helpers/issues/1
-    # def test_perform_if_exception(self):
-    #     class CustomException(Exception):
-    #         def __init__(self, text=None):
-    #             self.text = text
-    #
-    #     def perform_task(task):
-    #         raise CustomException(text="new custom ex text")
-    #
-    #     queue_name = "test_perform_if_exception"
-    #     task_data = "test_task_data"
-    #     task_id = self.task_courier.add_task_to_queue(
-    #         queue_name=queue_name,
-    #         task_data=task_data)
-    #
-    #     # monkey patching
-    #     self.base_worker.perform_task = perform_task
-    #     self.base_worker.queue_name = queue_name
-    #
-    #     self.base_worker.perform(total_tasks=1)
-    #     task_result = self.task_courier.wait_for_task_result(
-    #         queue_name=queue_name,
-    #         task_id=task_id)
-    #
-    #     self.assertEqual(type(task_result), exceptions.PerformTaskError)
-    #     self.assertEqual(type(task_result.exception), CustomException)
-    #     self.assertEqual(task_result.exception.text, "new custom ex text")
+            self.worker.perform_tasks(tasks=[(uuid.uuid1(), "task_data")])
