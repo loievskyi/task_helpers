ce7d3ed26a243ca7f4374e68ab693df9f200ab5e
Slava Loievskyi <loievskyi.slava@gmail.com>
2022-12-28 19:43:45 +0200

    Added task argument to PerformTaskError

--- task_helpers/exceptions.py
+++ task_helpers/exceptions.py
@@ -11,6 +11,7 @@ class TaskResultDoesNotExist(DoesNotExistError):
 
 
 class PerformTaskError(Exception):
-    def __init__(self, exception=None, error_data=None):
+    def __init__(self, exception=None, error_data=None, task=None):
+        self.task = task
         self.exception = exception
         self.error_data = error_data
