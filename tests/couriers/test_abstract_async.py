256a0b77c478dd9b4a050a4e14a99956a44b6732
Slava Loievskyi <loievskyi.slava@gmail.com>
2024-01-23 23:35:04 +0200

    Changes on tests for abstract async task courier

--- tests/couriers/test_abstract_async.py
+++ tests/couriers/test_abstract_async.py
@@ -1,6 +1,5 @@
 import uuid
 import unittest
-import asyncio
 
 from task_helpers.couriers.abstract_async import (
     AbstractAsyncClientTaskCourier,
@@ -9,108 +8,91 @@ from task_helpers.couriers.abstract_async import (
 )
 
 
-class AbstractAsyncClientTaskCourierTestCase(unittest.TestCase):
+class AbstractAsyncClientTaskCourierTestCase(unittest.IsolatedAsyncioTestCase):
     """
-    Tests to make sure that AbstractAsyncClientTaskCourier is working correctly.
+    Tests to make sure that AbstractAsyncClientTaskCourier
+    is working correctly.
     """
 
     def setUp(self):
         self.async_task_courier = AbstractAsyncClientTaskCourier()
 
-    def test_get_task_result(self):
+    async def test_get_task_result(self):
         with self.assertRaises(expected_exception=NotImplementedError):
-            asyncio.run(
-                self.async_task_courier.get_task_result(
-                    queue_name="queue_name",
-                    task_id=uuid.uuid1())
-            )
+            await self.async_task_courier.get_task_result(
+                queue_name="queue_name",
+                task_id=uuid.uuid1())
 
-    def test_wait_for_task_result(self):
+    async def test_wait_for_task_result(self):
         with self.assertRaises(expected_exception=NotImplementedError):
-            asyncio.run(
-                self.async_task_courier.wait_for_task_result(
-                    queue_name="queue_name",
-                    task_id=uuid.uuid1())
-            )
+            await self.async_task_courier.wait_for_task_result(
+                queue_name="queue_name",
+                task_id=uuid.uuid1())
 
-    def test_add_task_to_queue(self):
+    async def test_add_task_to_queue(self):
         with self.assertRaises(expected_exception=NotImplementedError):
-            asyncio.run(
-                self.async_task_courier.add_task_to_queue(
-                    queue_name="queue_name",
-                    task_data="task_data")
-            )
+            await self.async_task_courier.add_task_to_queue(
+                queue_name="queue_name",
+                task_data="task_data")
 
-    def test_bulk_add_tasks_to_queue(self):
+    async def test_bulk_add_tasks_to_queue(self):
         with self.assertRaises(expected_exception=NotImplementedError):
-            asyncio.run(
-                self.async_task_courier.bulk_add_tasks_to_queue(
-                    queue_name="queue_name",
-                    tasks_data=["task_data"])
-            )
+            await self.async_task_courier.bulk_add_tasks_to_queue(
+                queue_name="queue_name",
+                tasks_data=["task_data"])
 
-    def test_check_for_done(self):
+    async def test_check_for_done(self):
         with self.assertRaises(expected_exception=NotImplementedError):
-            asyncio.run(
-                self.async_task_courier.check_for_done(
-                    queue_name="queue_name",
-                    task_id=uuid.uuid1())
-            )
+            await self.async_task_courier.check_for_done(
+                queue_name="queue_name",
+                task_id=uuid.uuid1())
 
 
-class AbstractAsyncWorkerTaskCourierTestCase(unittest.TestCase):
+class AbstractAsyncWorkerTaskCourierTestCase(unittest.IsolatedAsyncioTestCase):
     """
-    Tests to make sure that AbstractAsyncWorkerTaskCourier is working correctly.
+    Tests to make sure that AbstractAsyncWorkerTaskCourier
+    is working correctly.
     """
 
     def setUp(self):
         self.async_task_courier = AbstractAsyncWorkerTaskCourier()
 
-    def test_bulk_get_tasks(self):
+    async def test_bulk_get_tasks(self):
         with self.assertRaises(expected_exception=NotImplementedError):
-            asyncio.run(
-                self.async_task_courier.bulk_get_tasks(
-                    queue_name="queue_name",
-                    max_count=100,
-                )
+            await self.async_task_courier.bulk_get_tasks(
+                queue_name="queue_name",
+                max_count=100,
             )
 
-    def test_get_task(self):
+    async def test_get_task(self):
         with self.assertRaises(expected_exception=NotImplementedError):
-            asyncio.run(
-                self.async_task_courier.get_task(queue_name="queue_name")
-            )
+            await self.async_task_courier.get_task(queue_name="queue_name")
 
-    def test_wait_for_task(self):
+    async def test_wait_for_task(self):
         with self.assertRaises(expected_exception=NotImplementedError):
-            asyncio.run(
-                self.async_task_courier.wait_for_task(queue_name="queue_name")
-            )
+            await self.async_task_courier.wait_for_task(
+                queue_name="queue_name")
 
-    def test_return_task_result(self):
+    async def test_return_task_result(self):
         with self.assertRaises(expected_exception=NotImplementedError):
-            asyncio.run(
-                self.async_task_courier.return_task_result(
-                    queue_name="queue_name",
-                    task_id=uuid.uuid1(),
-                    task_result="task_result")
-            )
+            await self.async_task_courier.return_task_result(
+                queue_name="queue_name",
+                task_id=uuid.uuid1(),
+                task_result="task_result")
 
-    def test_bulk_return_task_results(self):
+    async def test_bulk_return_task_results(self):
         with self.assertRaises(expected_exception=NotImplementedError):
             task = (uuid.uuid1(), "task_result")
-            asyncio.run(
-                self.async_task_courier.bulk_return_task_results(
-                    queue_name="queue_name",
-                    tasks=[task]
-                )
+            await self.async_task_courier.bulk_return_task_results(
+                queue_name="queue_name",
+                tasks=[task]
             )
 
 
 class AbstractAsyncClientWorkerTaskCourierTestCase(
         AbstractAsyncClientTaskCourierTestCase,
         AbstractAsyncWorkerTaskCourierTestCase,
-        unittest.TestCase):
+        unittest.IsolatedAsyncioTestCase):
     """
     Tests to make sure that AbstractAsyncClientWorkerTaskCourier
     is working correctly.
