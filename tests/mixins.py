5cb9fbe3cf7e9276bef7ce7cc0d03be0847da68c
Slava Loievskyi <loievskyi.slava@gmail.com>
2024-02-11 01:44:12 +0200

    Added asyncSetUp and asyncTearDown methods in RedisSetupMixin

--- tests/mixins.py
+++ tests/mixins.py
@@ -19,18 +19,28 @@ class RedisSetupMixin:
         assert redis_port is not None, "redis_port is None"
         assert redis_db is not None, "redis_db is None"
 
-        redis_connection = redis.Redis(
+        self.redis_connection = redis.Redis(
             host=redis_host,
             port=redis_port,
             db=redis_db,
             password=redis_password)
-
-        aioredis_connection = aioredis.Redis(
-            host=redis_host,
-            port=redis_port,
-            db=redis_db,
-            password=redis_password)
-
-        self.redis_connection = redis_connection
-        self.aioredis_connection = aioredis_connection
         self.redis_connection.flushdb()
+
+    async def asyncSetUp(self):
+        redis_host = os.environ.get("REDIS_HOST")
+        redis_port = os.environ.get("REDIS_PORT")
+        redis_db = os.environ.get("REDIS_DB")
+        redis_password = os.environ.get("REDIS_PASSWORD", None)
+
+        assert redis_host is not None, "redis_host is None"
+        assert redis_port is not None, "redis_port is None"
+        assert redis_db is not None, "redis_db is None"
+
+        self.aioredis_connection = aioredis.Redis(
+            host=redis_host,
+            port=redis_port,
+            db=redis_db,
+            password=redis_password)
+
+    async def asyncTearDown(self):
+        await self.aioredis_connection.aclose()
