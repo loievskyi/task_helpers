76a63f148845a4a32d52f22055ff41b7fd7dad6f
Slava Loievskyi <loievskyi.slava@gmail.com>
2025-06-20 16:36:09 +0300

    Add `Serializer` base class with generic interface for serialization and deserialization

---
+++ task_helpers/serializers/base.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+from typing import TypeVar, Generic
+
+SourceType = TypeVar("SourceType")
+TargetType = TypeVar("TargetType")
+
+
+class Serializer(ABC, Generic[SourceType, TargetType]):
+    """Base serializer interface with generic source and target types"""
+
+    @abstractmethod
+    def serialize(self, data: SourceType) -> TargetType:
+        """Convert source data to the target format"""
+        pass
+
+    @abstractmethod
+    def deserialize(self, data: TargetType) -> SourceType:
+        """Restore source data from target format"""
+        pass
