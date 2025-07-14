b5311cb96dab974378ef5f3870d7666de7bd57b6
Slava Loievskyi <loievskyi.slava@gmail.com>
2025-07-14 23:46:46 +0300

    Enhance `PerformTaskError` with detailed exception data and traceback handling
    - Added `_get_exception_data` method to capture class name, module name, message, and traceback of the exception.
    - Replaced `error_data` with `exception_data` to provide richer diagnostics for task errors.

--- task_helpers/exceptions.py
+++ task_helpers/exceptions.py
@@ -1,4 +1,4 @@
-from typing import Any, Type
+import traceback
 
 from task_helpers.tasks import Task
 
@@ -20,9 +20,22 @@ class TaskResultDoesNotExist(DoesNotExistError):
 
 
 class PerformTaskError(TaskHelperError):
-    def __init__(self, exception: Exception | Type[Exception] = None,
-                 error_data: Any = None,
+    def __init__(self, exception: Exception | None = None,
                  task: Task | tuple | None = None):
         self.exception = exception
-        self.error_data = error_data
+        self.exception_data = self._get_exception_data(exception)
         self.task = task
+
+    def _get_exception_data(self, exception: Exception):
+        return {
+            "class_name": exception.__class__.__name__,
+            "module_name": exception.__class__.__module__,
+            "message": str(exception),
+            "traceback": self._get_traceback_or_none(exception),
+        }
+
+    def _get_traceback_or_none(self, exception: Exception) -> str | None:
+        traceback_str = None
+        if hasattr(exception, "__traceback__") and exception.__traceback__:
+            traceback_str = "".join(traceback.format_tb(exception.__traceback__))
+        return traceback_str
