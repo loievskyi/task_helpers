1cda5c7f1bf38b5836b5f46f60bc63e9a4dd90f1
Slava Loievskyi <loievskyi.slava@gmail.com>
2025-07-17 17:26:54 +0300

    Enhance `TaskResultSerializer` to handle `PerformTaskError` serialization and deserialization
    - Added support for encoding and decoding `PerformTaskError` using the new `PerformTaskErrorTupleConverter`.
    - Introduced a flag byte to differentiate error and non-error serialized data.
    - Updated serialization and deserialization logic to handle the new error flag.

--- task_helpers/serializers/task_result.py
+++ task_helpers/serializers/task_result.py
@@ -3,6 +3,8 @@ from typing import Any
 from task_helpers.compressors import Compressor
 from task_helpers.converters import BytesConverter
 from .base import Serializer
+from ..converters.perform_task_error_tuple import PerformTaskErrorTupleConverter
+from ..exceptions import PerformTaskError
 
 
 class TaskResultSerializer(Serializer[Any, bytes]):
@@ -15,6 +17,7 @@ class TaskResultSerializer(Serializer[Any, bytes]):
     ):
         self._bytes_converter = bytes_converter
         self._compressor = compressor
+        self._perform_task_error_converter = PerformTaskErrorTupleConverter()
 
     def serialize(self, task_result: Any) -> bytes:
         """
@@ -24,9 +27,17 @@ class TaskResultSerializer(Serializer[Any, bytes]):
         1. Converting an object to bytes
         2. Compressing bytes
         """
+
+        is_error = isinstance(task_result, PerformTaskError)
+
+        if is_error:
+            task_result = self._perform_task_error_converter.encode(task_result)
+
         bytes_data = self._bytes_converter.encode(task_result)
         compressed_data = self._compressor.compress(bytes_data)
-        return compressed_data
+
+        flag_byte = b"\x01" if is_error else b"\x00"
+        return flag_byte + compressed_data
 
     def deserialize(self, data: bytes) -> Any:
         """
@@ -36,6 +47,15 @@ class TaskResultSerializer(Serializer[Any, bytes]):
         1. Decompressing bytes
         2. Restoring an object from decompressed data
         """
-        decompressed_data = self._compressor.decompress(data)
+
+        flag_byte = data[0:1]
+        is_error = flag_byte == b"\x01"
+        compressed_data = data[1:]
+
+        decompressed_data = self._compressor.decompress(compressed_data)
         decoded_data = self._bytes_converter.decode(decompressed_data)
+
+        if is_error:
+            decoded_data = self._perform_task_error_converter.decode(decoded_data)
+
         return decoded_data
