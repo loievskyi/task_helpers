5b11b4c1903c1a2be224f6362d85550f2f88f75b
Slava Loievskyi <loievskyi.slava@gmail.com>
2025-08-16 23:48:04 +0300

    Remove unused backend fixtures and replace `TaskResultSerializer` with `CustomTypeSerializer` in `conftest.py`

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,24 +6,18 @@ import string
 import threading
 import time
 import uuid
-from enum import Enum
 from typing import Callable, Type, Any, AsyncGenerator, Generator
-
-from task_helpers.backends.async_ import AsyncBackend, AsyncRedisBackend
-from task_helpers.exceptions import DoesNotExistError
 
 import pytest
 import pytest_asyncio
 import redis
 import redis.asyncio as aioredis
 
-from task_helpers.backends.sync import Backend
-from task_helpers.backends.sync import RedisBackend
 from task_helpers.compressors.core.base import LeveledCompressor, Compressor
 from task_helpers.converters import TaskTupleConverter, BytesConverter
 from task_helpers.serializers.base import Serializer
+from task_helpers.serializers.custom_type import CustomTypeSerializer
 from task_helpers.serializers.task import TaskSerializer
-from task_helpers.serializers.task_result import TaskResultSerializer
 from task_helpers.tasks import Task
 
 
@@ -125,8 +119,8 @@ def mock_task_serializer(mock_task_tuple_converter, mock_bytes_converter, mock_c
 
 
 @pytest.fixture
-def mock_task_result_serializer(mock_bytes_converter, mock_compressor) -> TaskResultSerializer:
-    return TaskResultSerializer(
+def mock_task_result_serializer(mock_bytes_converter, mock_compressor) -> CustomTypeSerializer:
+    return CustomTypeSerializer(
         bytes_converter=mock_bytes_converter,
         compressor=mock_compressor,
     )
@@ -176,53 +170,6 @@ async def mock_aioredis_client() -> AsyncGenerator[aioredis.Redis, None]:
     yield client
     await client.close()
     await client.connection_pool.disconnect()
-
-
-class SyncBackendType(Enum):
-    REDIS = RedisBackend
-
-
-class AsyncBackendType(Enum):
-    REDIS = AsyncRedisBackend
-
-
-@pytest.fixture
-def backend(request, mock_redis_client: redis.Redis) -> Backend:
-    """Create a clean Backend instance for each test"""
-    backend_type: SyncBackendType = request.param
-
-    if backend_type == SyncBackendType.REDIS:
-        mock_redis_client.flushdb()
-        return RedisBackend(mock_redis_client)
-    raise ValueError(f"Invalid backend type: {backend_type}")
-
-
-@pytest_asyncio.fixture
-async def async_backend(request, mock_aioredis_client: aioredis.Redis) -> AsyncBackend:
-    """Create a clean AsyncBackend instance for each test"""
-    backend_type: AsyncBackendType = request.param
-
-    if backend_type == AsyncBackendType.REDIS:
-        await mock_aioredis_client.flushdb()
-        return AsyncRedisBackend(mock_aioredis_client)
-    raise ValueError(f"Invalid backend type: {backend_type}")
-
-
-def pytest_generate_tests(metafunc):
-    if "backend" in metafunc.fixturenames:
-        metafunc.parametrize(
-            "backend",
-            list(SyncBackendType),
-            indirect=True,
-            ids=[backend_type.name for backend_type in SyncBackendType]
-        )
-    elif "async_backend" in metafunc.fixturenames:
-        metafunc.parametrize(
-            "async_backend",
-            list(AsyncBackendType),
-            indirect=True,
-            ids=[backend_type.name for backend_type in AsyncBackendType]
-        )
 
 
 def assert_blocks_longer_than(seconds: float, timeout: float = None):
