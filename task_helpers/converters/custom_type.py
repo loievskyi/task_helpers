5e5d0070909e6964571acf7c478aa2fd4d74402f
Slava Loievskyi <loievskyi.slava@gmail.com>
2025-08-20 21:21:22 +0300

    Refactor `CustomTypeConverter` to simplify prefix mapping and converter management
    - Replaced manual type-prefix mapping with `_add_converter` method for dynamic prefix management.
    - Introduced `_default_prefix` for fallback handling in `encode`.
    - Removed redundant `set_task_converter` method.

--- task_helpers/converters/custom_type.py
+++ task_helpers/converters/custom_type.py
@@ -14,24 +14,18 @@ class CustomTypeConverter(Converter[Any, tuple[bytes, Any]]):
     def __init__(self, perform_task_error_converter: PerformTaskErrorTupleConverter,
                  task_converter: TaskTupleConverter,
                  converter_stub: ConverterStub):
-        self._perform_task_error_converter = perform_task_error_converter
-        self._task_converter = task_converter
-        self._converter_stub = converter_stub
-
-        self._type_prefix_map = {
-            "default": b"\x00",
-            PerformTaskError: b"\x01",
-            Task: b"\x02",
+        self._default_prefix = b"\x00"
+        self._type_prefix_map: dict[type, bytes] = dict()
+        self._prefix_encoders_map: dict[bytes, Converter] = {
+            self._default_prefix: converter_stub
         }
 
-        self._prefix_encoders_map = {
-            b"\x00": self._converter_stub,
-            b"\x01": self._perform_task_error_converter,
-            b"\x02": self._task_converter,
-        }
+        self._add_converter(Task, task_converter)
+        self._add_converter(PerformTaskError, perform_task_error_converter)
 
     def encode(self, source: Any) -> tuple[bytes, Any]:
-        byte_prefix = self._type_prefix_map.get(type(source), b"\x00")
+        byte_prefix = self._type_prefix_map.get(
+            type(source), self._default_prefix)
         converter = self._prefix_encoders_map[byte_prefix]
         encoded = converter.encode(source)
         return byte_prefix, encoded
@@ -41,5 +35,10 @@ class CustomTypeConverter(Converter[Any, tuple[bytes, Any]]):
         converter = self._prefix_encoders_map[byte_prefix]
         return converter.decode(encoded)
 
-    def set_task_converter(self, task_converter: TaskTupleConverter):
-        self._task_converter = task_converter
+    def _add_converter(self, source_type: type, converter: Converter):
+        int_prefixes = [int.from_bytes(value) for value in self._prefix_encoders_map]
+        new_prefix = max(int_prefixes) + 1
+        new_prefix = new_prefix.to_bytes(self.prefix_size, "big")
+
+        self._type_prefix_map[source_type] = new_prefix
+        self._prefix_encoders_map[new_prefix] = converter
