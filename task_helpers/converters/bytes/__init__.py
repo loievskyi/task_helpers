28c8cad57104edc927fc99b0c234ebb204a26b5b
Slava Loievskyi <loievskyi.slava@gmail.com>
2025-09-13 15:24:56 +0300

    Temporarily remove not tested bytes converters (`AvroConverter`, `BsonConverter`, `CsvConverter`, `JsonConverter`, `ProtobufConverter`) and update `__all__` exports of `bytes` module.

--- task_helpers/converters/bytes/__init__.py
+++ task_helpers/converters/bytes/__init__.py
@@ -1,20 +1,10 @@
-from .avro import AvroConverter
 from .base import BytesConverter
-from .bson import BsonConverter
-from .csv import CsvConverter
-from .json import JsonConverter
 from .msgpack import MsgPackConverter
 from .pickle import PickleConverter
-from .protobuf import ProtobufConverter
 
 __all__ = [
     "BytesConverter",
 
-    "AvroConverter",
-    "BsonConverter",
-    "CsvConverter",
-    "JsonConverter",
     "MsgPackConverter",
     "PickleConverter",
-    "ProtobufConverter",
 ]
